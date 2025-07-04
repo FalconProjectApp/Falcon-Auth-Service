f72f14b9008aeae40d0af233be3c0968b2f872b9
Gustavo Santarosa <bolota_xd@hotmail.com>
2025-07-04 13:59:45 -0300

    Started the auth microservice.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->configureTelescope();
     }
 
     /**
@@ -21,6 +22,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(!app()->isProduction());
+    }
+
+    protected function configureTelescope(): void
+    {
+        if (in_array(config('app.env'), explode(',', (string) config('telescope.enabled_env')), true)) {
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 }
